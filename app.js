7e19c821151bdf24cec9acced1e5fa0fd3e7a2e8
Mei Deng <meideng06@gmail.com>
2022-07-28 19:46:53 -0400

    countdown

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const countdown = () => {
+    const countDate = new Date('October 14, 2022 00:00:00').getTime()
+    const now = new Date().getTime()
+    const gap = countDate - now
 
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    // calculate countdown
+    const textDay = Math.floor(gap / day)
+    const textHour = Math.floor((gap % day) / hour)
+    const textMin = Math.floor((gap % hour) / minute)
+    const textSec = Math.floor((gap % minute) / second)
+
+    document.querySelector('.day').innerText = textDay
+    document.querySelector('.hour').innerText = textHour
+    document.querySelector('.min').innerText = textMin
+    document.querySelector('.sec').innerText = textSec
+}
+
+setInterval(countdown, 1000)
